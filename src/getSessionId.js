7cd384417bf7e4ce4c4263219bb12fd7a4b36345
Riley Shaw <rileyjshaw@gmail.com>
2019-02-11 15:42:53 -0800

    Improve session ID algorithm

--- src/getSessionId.js
+++ src/getSessionId.js
@@ -1,7 +1,5 @@
 // Returns a calm, memorable session ID that's not already in `localStorage`.
 
-const randomFrom = arr => arr[Math.floor(Math.random() * arr.length)];
-
 const adjectives = [
 	'mossy',
 	'moonlit',
@@ -21,13 +19,29 @@ const nouns = [
 ];
 
 export default function getSession () {
-	let adjective = '', noun = '', sessionId = '';
+	const nAdjectives = adjectives.length, nNouns = nouns.length;
 
+	const initialAdjectiveIdx = Math.floor(Math.random() * nAdjectives);
+	const initialNounIdx = Math.floor(Math.random() * nNouns);
+
+	for (let adjectiveOffset = 0; adjectiveOffset < nAdjectives; ++adjectiveOffset) {
+		for (let nounOffset = 0; nounOffset < nNouns; ++nounOffset) {
+			const adjectiveIdx = (initialAdjectiveIdx + adjectiveOffset) % nAdjectives;
+			const nounIdx = (initialNounIdx + nounOffset) % nNouns;
+			const adjective = adjectives[adjectiveIdx];
+			const noun = nouns[nounIdx];
+			const sessionId = `${adjective}-${noun}`;
+
+			if (!window.localStorage.getItem(sessionId)) return sessionId;
+		}
+	}
+
+	// We've exhausted all possibilities. Now we start into the numbered IDs.
+	const sessionPrefix = `${adjectives[initialAdjectiveIdx]}-${nouns[initialNounIdx]}`;
+	let sessionId = 0, n = 1;
 	do {
-		adjective = randomFrom(adjectives);
-		noun = randomFrom(nouns);
-		sessionId = `${adjective}-${noun}`
-	} while (window.localStorage.getItem(sessionId));
+		sessionId = `${sessionPrefix}-${++n}`
+	} while (!window.localStorage.getItem(sessionId));
 
 	return sessionId;
 }
